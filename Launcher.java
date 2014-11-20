aedfdd42330799601094089f565e274e5e32e214
Valerson <thevaleyard92@gmail.com>
2014-11-20 07:02:16 -0500

    Create Launcher.java

---
+++ Launcher.java
@@ -0,0 +1,11 @@
+
+//Assumably, the player will launch the TTT game by doubleclicking a batch file which tells java to run this file.
+public class Launcher
+{
+public static void main(String[] args)
+  {
+
+  }
+
+
+}
