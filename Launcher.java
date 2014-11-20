7d7b7f01aad3f0433fa02e7ac02e22269d9e1917
vmascari <grc@mascari.org>
2014-11-20 11:45:17 -0500

    Corrected comments / added comments.

--- Launcher.java
+++ Launcher.java
@@ -1,5 +1,8 @@
 
-//Assumably, the player will launch the TTT game by doubleclicking a batch file which tells java to run this file.
+// Assumably, the player will launch the TTT game by doubleclicking the JAR file.  Java is assumed to be on the system.
+
+// The Launcher has two modes:  Client only or Client + Server.
+
 public class Launcher
 {
 public static void main(String[] args)
