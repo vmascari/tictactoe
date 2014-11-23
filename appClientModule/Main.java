ec860081e5225e797b51a86c8a23135362f4e7e6
Schmittywerberyangermanjensen <nmascari44@gmail.com>
2014-11-22 19:25:34 -0500

    made change

--- appClientModule/Main.java
+++ appClientModule/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, World!");
+		System.out.println("Hello, World from Schmittywerberyangermanjensen!");
 		System.out.println("And Nick is stupid.");
 	}
 
